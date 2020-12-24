f6a4cdff80f3862b3369a336a9103f396591fb70
Han <zh950713@gmail.com>
2020-12-24 16:39:22 +0800

    Finished test

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# Python Project Template
+# Test PyTorch DataParallel and DistributedDataParallel
 
-This is a sample Python project template.
+To run the test:
+
+```bash
+$ python test_parallel/single_gpu.py
+$ python test_parallel/data_parallel.py
+$ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node=8 test_parallel/distributed_parallel.py
+```
--- test_parallel/distributed_parallel.py
+++ test_parallel/distributed_parallel.py
@@ -1,2 +1,53 @@
+import time
+import torch
 import torch.nn as nn
+import torch.optim as optim
 import torch.distributed as dist
+from torch.utils.data import DataLoader
+from torch.utils.data.distributed import DistributedSampler
+
+from data import TestDataSet
+from model import TestNet
+from utils.logging import LoggingModule
+
+
+class Logger(LoggingModule):
+    pass
+
+
+batch_size = 80
+nlayers = 5
+ndata = 500000
+dmodel = 1000
+lr = 0.1
+
+dist.init_process_group(backend='nccl')
+local_rank = torch.distributed.get_rank()
+torch.cuda.set_device(local_rank)
+device = torch.device("cuda", local_rank)
+
+dataset = TestDataSet(ndata, dmodel)
+sampler = DistributedSampler(dataset)
+dataloader = DataLoader(dataset, batch_size, sampler=sampler)
+model = nn.parallel.DistributedDataParallel(
+    TestNet(nlayers, dmodel).to(device),
+    device_ids=[local_rank],
+    output_device=local_rank
+)
+optimizer = optim.SGD(model.parameters(), lr)
+logger = Logger('data parallel')
+
+logger.info('Run on {} gpus with batchsize = {}.'.format(
+    torch.cuda.device_count(), batch_size
+))
+start_timestamp = time.time()
+
+# Forward and backward pass
+for batch in dataloader:
+    optimizer.zero_grad()
+    batch = batch.to(device)
+    result = model(batch)
+    result.mean().backward()
+
+logger.info('Time ellapsed: {}s'.format(time.time() - start_timestamp))
+
